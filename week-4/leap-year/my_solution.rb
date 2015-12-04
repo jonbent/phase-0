fcb5f2fd6fe698813a9c99db4048bec8c3074d01
Todd Seller <me@toddseller.com>
2015-12-03 19:15:36 -0800

    Add my_solution.rb file and solution to leap-year directory

---
+++ week-4/leap-year/my_solution.rb
@@ -0,0 +1,14 @@
+# Leap Years
+
+# I worked on this challenge with: Lauren Reid.
+
+
+# Your Solution Below
+def leap_year?(year)
+  if year % 100 == 0 && year % 400 != 0
+    return false
+  elsif year % 4 == 0 || year % 400 == 0
+    return true
+  end
+  return false
+end
